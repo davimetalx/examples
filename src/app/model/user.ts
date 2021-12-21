433374250c5988ad89fd91b6b6b87c66cc6f847a
Davi Nascimento da Silva <davinascimentodasilva10@gmail.com>
2021-12-20 23:22:50 -0300

    Change model

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,9 +1,9 @@
 export class User {
-    readonly id: number = 0;
-    firstName: string = '';
-    lastName: string = '';
-    username: string = '';
-    email: string = '';
-    phone: string = '';
-    password: string = '';
+    readonly id?: number;
+    firstName?: string;
+    lastName?: string;
+    username?: string;
+    email?: string;
+    phone?: string;
+    password?: string;
 }
